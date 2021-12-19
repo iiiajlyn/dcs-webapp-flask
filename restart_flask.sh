d8f68e233be5686d2579af5310c6f04f53a65b9a
Michel Koyang-Kuleshov <michelkoyangkuleshov@gmail.com>
2021-12-19 19:18:03 +0100

    Added restart_flask.sh

--- restart_flask.sh
+++ restart_flask.sh
@@ -1,3 +1,9 @@
+echo "!!!!!!!!!!!!" current date "!!!!!!!!!"
+date
+echo "!!!!!!!!!!!!" current date "!!!!!!!!!"
+cd /home/dcs-file5tat/dcs-webapp-flask
+echo "current dir: " $PWD
 docker-compose build
+docker-compose down
 docker-compose up -d
 docker rmi $(docker images --filter "dangling=true")
